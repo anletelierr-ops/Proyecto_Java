5a83f6de82f637579bf343127d96f2ec19fbb8d9
anletelierr-ops <an.letelierr@duocuc.cl>
2026-09-07 19:57:23 -0300

    JAVA-011 - Agregar menu con while

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,14 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        int opcion = -1;
+
+        while (opcion != 0) {
+
+            System.out.println("=== SISTEMA DUOC ===");
+            System.out.println("1. Mostrar estado");
+            System.out.println("2. Procesar operación");
+            System.out.println("0. Salir");
         System.out.print("Ingrese nombre: ");
         String nombre = scanner.nextLine();
 
